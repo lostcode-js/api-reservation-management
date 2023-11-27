cd0992aaede5b49365f858881dc433d460cbe8c7
Daniel Soares <daniel.soares@fortics.com.br>
2023-11-26 21:36:39 -0300

    fix: adjsut cors.

--- index.js
+++ index.js
@@ -7,22 +7,10 @@ require('dotenv').config()
 const app = express()
 const port = process.env.APP_PORT ?? '3000';
 
-/*
-app.listen(port, () => {
-  console.log(`Example app listen por ${port}`)
-})
-*/
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(cors({
-  origin: [
-    'https://lp-drip-barber-peach.vercel.app/',
-    'https://dripbr.pt/',
-    'https://lp-drip-barber-drip-barbers-projects.vercel.app/',
-    'https://lp-drip-barber-git-main-drip-barbers-projects.vercel.app/'
-  ],
   methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
 }));
 
@@ -31,8 +19,6 @@ app.use('/api', require('./src/routes/index.js'))
 app.get("/", (req, res) => {
   res.send("Hello World!")
 })
-
-
 
 Promise.resolve()
   .then(initDatabase)
