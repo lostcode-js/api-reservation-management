e3963ea18af9df9bfe7e5adc75eb421e0a6340bb
Daniel Soares <daniel.soares@fortics.com.br>
2023-11-26 21:26:32 -0300

    fix: change cors.

--- index.js
+++ index.js
@@ -17,8 +17,13 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(cors({
-    origin: ['*'],
-    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+  origin: [
+    'https://lp-drip-barber-peach.vercel.app/',
+    'https://dripbr.pt/',
+    'https://lp-drip-barber-drip-barbers-projects.vercel.app/',
+    'https://lp-drip-barber-git-main-drip-barbers-projects.vercel.app/'
+  ],
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
 }));
 
 app.use('/api', require('./src/routes/index.js'))
@@ -30,11 +35,11 @@ app.get("/", (req, res) => {
 
 
 Promise.resolve()
-.then(initDatabase)
-.then(() => new Promise((resolve, reject) => {
-  const server = require('http').createServer(app).listen(port, '0.0.0.0', err =>
-    err ? reject(err) : resolve(server)
-  )
-}))
-.then(() => console.log('Servidor iniciado com sucesso!', `porta: ${port}`))
-.catch((error) => console.error("Erro ao iniciar servidor", error))
+  .then(initDatabase)
+  .then(() => new Promise((resolve, reject) => {
+    const server = require('http').createServer(app).listen(port, '0.0.0.0', err =>
+      err ? reject(err) : resolve(server)
+    )
+  }))
+  .then(() => console.log('Servidor iniciado com sucesso!', `porta: ${port}`))
+  .catch((error) => console.error("Erro ao iniciar servidor", error))
