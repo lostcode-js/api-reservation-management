516fe393ea9b8a3c388d705cc0b7ebf4ba877ce4
Daniel Soares <daniel.soares@fortics.com.br>
2023-12-29 23:01:00 -0300

    fix: Add route of avatar.

--- api/src/controllers/avatarController.js
+++ api/src/controllers/avatarController.js
@@ -5,12 +5,12 @@ exports.upload = async (req, res) => {
     try {
         parser.single('avatar')(req, res, err => {
             if (err)
-                res.status(500).json({ error: 1, payload: err });
+                res.status(500).json({ message: err });
             else {
                 const image = {};
                 image.id = req.file.filename;
                 image.url = `/uploads/${image.id}`;
-                res.status(200).json({ error: 0, payload: { id: image.id, url: image.url } });
+                res.status(200).json({ message: 'Upload da imagem com sucesso', avatar: { id: image.id, url: image.url } });
             }
         });
     } catch (error) {
--- api/src/routes/avatarRoute.js
+++ api/src/routes/avatarRoute.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/avatarController')
 
-
 router.post('/', requireAuth, controller.upload);
 
 module.exports = router;
